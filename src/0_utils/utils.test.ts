7a05b66da682b7773120b5bab2948d0a8af36dc0
tylim <limteikyee@gmail.com>
2021-10-14 10:46:22 +0800

    delete extra code

--- src/0_utils/utils.test.ts
+++ src/0_utils/utils.test.ts
@@ -2,8 +2,6 @@ import 'jest'
 import { onlyAlphaNumeric } from './0_onlyAlphaNumeric'
 import { swapKeyAndValue } from './0_swapKeyAndValue'
 import { trimAndLowerCase } from './0_trimAndLowerCase'
-import { onlyAlphaNumericKey } from './1_onlyAlphaNumericKey'
-import { trimAndLowerKeyCase } from './1_trimAndLowerKeyCase'
 
 describe('0_utils', () => {
 	describe('0_onlyAlphaNumeric', () => {
@@ -28,24 +26,6 @@ describe('0_utils', () => {
 		it('test with < sd U&Y-P  >', () => {
 			expect.assertions(1)
 			expect(trimAndLowerCase(' sd U&Y-P  ')).toEqual('sd u&y-p')
-		})
-	})
-
-	describe('1_onlyAlphaNumericKey', () => {
-		it('test with < {"sd U&Y-P":1,"sjs 2&%^ksaui2":2}  >', () => {
-			expect.assertions(1)
-			expect(
-				onlyAlphaNumericKey({ 'sd U&Y-P': '1', 'sjs 2&B%^kS_a?ui2': '2' })
-			).toEqual({ sjs2bksaui2: '2', sduyp: '1' })
-		})
-	})
-
-	describe('1_trimAndLowerKeyCase', () => {
-		it('test with < {"sd U&Y-P":1,"sjs 2&%^ksaui2":2}  >', () => {
-			expect.assertions(1)
-			expect(
-				trimAndLowerKeyCase({ ' sd U&Y-P ': '1', ' sjs 2&B%^kS_a?ui2 ': '2' })
-			).toEqual({ 'sjs 2&b%^ks_a?ui2': '2', 'sd u&y-p': '1' })
 		})
 	})
 })
