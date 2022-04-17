fb76101d1e991c6c3c7bfaf14e6089c5349e0a08
tylim <limteikyee@gmail.com>
2022-04-17 13:50:58 +0800

    housekeeping

--- src/1_sync/0_fetch.ts
+++ src/1_sync/0_fetch.ts
@@ -4,9 +4,8 @@
  * @returns processed string
  */
 export const fetch = async (apiKey: string) => {
-	const axios = await import('axios')
-	// @ts-expect-error
-	const res = await axios.get(
+	const axios = (await import('axios')).default
+	const res = await axios(
 		'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5000',
 		{ headers: { 'X-CMC_PRO_API_KEY': apiKey } }
 	)
