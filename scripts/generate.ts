c3d0346b78a99a8f847efcd26383af50ec2a6af4
tylim <limteikyee@gmail.com>
2022-04-17 19:50:09 +0800

    working on crontask

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -2,22 +2,47 @@ import 'dotenv/config'
 import { fetch } from '../src/1_sync'
 import fs from 'fs'
 import path from 'path'
+import pkg from '../package.json'
+// @ts-expect-error
+import bumpVersion from 'semver-increment'
+import { diff } from 'deep-object-diff'
+import { symbolLookupObject } from '../src/0_constants'
+const masks = [0, 0, 1]
 
 export const updateConstant = async () => {
 	const data = await fetch(process.env.COINMARKETCAP_KEY as string)
-	const code =
-		`export const symbolLookupObject = {` +
-		data.reduce<string>((acc, coin) => {
-			acc =
-				acc +
-				`
-	[\`${coin.name}\`]: '${coin.symbol}',`
+	const newSymbolLookupObject = data.reduce<Record<string, string>>(
+		(acc, coin) => {
+			acc[coin.name] = coin.symbol
 			return acc
-		}, '') +
-		`
+		},
+		{}
+	)
+	const diffObj = diff(newSymbolLookupObject, symbolLookupObject)
+	if (Object.keys(diffObj).length > 0) {
+		const code =
+			`export const symbolLookupObject = {` +
+			data.reduce<string>((acc, coin) => {
+				acc =
+					acc +
+					`
+	[\`${coin.name}\`]: '${coin.symbol}',`
+				return acc
+			}, '') +
+			`
 } as const
 `
 
-	fs.writeFileSync(path.resolve(__dirname, '../src/0_constants/index.ts'), code)
+		fs.writeFileSync(
+			path.resolve(__dirname, '../src/0_constants/index.ts'),
+			code
+		)
+
+		pkg.version = bumpVersion(masks, pkg.version)
+		fs.writeFileSync(
+			path.resolve(__dirname, '../package.json'),
+			JSON.stringify(pkg)
+		)
+	}
 }
 updateConstant()
