05f2216b54d57e8363edcc3f94f1ade6ea81ec35
tylim <limteikyee@gmail.com>
2022-04-18 16:13:06 +0800

    update list

--- scripts/cron.ts
+++ scripts/cron.ts
@@ -1,19 +1,19 @@
 import shell from 'shelljs'
 import cron from 'cron'
-shell.exec(`npm run update`)
-// const job = new cron.CronJob(
-// 	'0 0 * * * *',
-// 	() => {
-// 		try {
-// 			shell.exec(`npm run update`)
-// 			console.log('cron task running')
-// 		} catch (e) {
-// 			console.log(e)
-// 		}
-// 	},
-// 	null,
-// 	true,
-// 	'Asia/Kuala_Lumpur'
-// )
 
-// job.start()
+const job = new cron.CronJob(
+	'0 0 * * * *',
+	() => {
+		try {
+			shell.exec(`npm run update`)
+			console.log('cron task running')
+		} catch (e) {
+			console.log(e)
+		}
+	},
+	null,
+	true,
+	'Asia/Kuala_Lumpur'
+)
+
+job.start()
