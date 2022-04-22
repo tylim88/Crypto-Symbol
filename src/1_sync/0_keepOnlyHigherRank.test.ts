9974ec217c2b6266f48884bfdac9105c463d6cd2
tylim <limteikyee@gmail.com>
2022-04-23 02:25:01 +0800

    100% test coverage

--- src/1_sync/0_keepOnlyHigherRank.test.ts
+++ src/1_sync/0_keepOnlyHigherRank.test.ts
@@ -1,21 +1,41 @@
 import { keepOnlyHigherRank } from './0_keepOnlyHigherRank'
 
-it('test keep only higher rank', () => {
-	expect(
-		keepOnlyHigherRank([
-			{ name: 'a', symbol: 'A', cmc_rank: 2 },
+describe('test keep only higher rank', () => {
+	it('ascending order', () => {
+		expect(
+			keepOnlyHigherRank([
+				{ name: 'a', symbol: 'A', cmc_rank: 2 },
+				{ name: 'b', symbol: 'B', cmc_rank: 3 },
+				{ name: 'B', symbol: 'u', cmc_rank: 4 },
+				{ name: 'Z', symbol: 'U', cmc_rank: 5 },
+				{ name: 'Y', symbol: 'b', cmc_rank: 50 },
+			])
+		).toEqual([
+			{
+				name: 'a',
+				symbol: 'A',
+				cmc_rank: 2,
+			},
 			{ name: 'b', symbol: 'B', cmc_rank: 3 },
-			{ name: 'B', symbol: 'u', cmc_rank: 4 },
 			{ name: 'Z', symbol: 'U', cmc_rank: 5 },
-			{ name: 'Y', symbol: 'b', cmc_rank: 50 },
 		])
-	).toEqual([
-		{
-			name: 'a',
-			symbol: 'A',
-			cmc_rank: 2,
-		},
-		{ name: 'b', symbol: 'B', cmc_rank: 3 },
-		{ name: 'Z', symbol: 'U', cmc_rank: 5 },
-	])
+	})
+	it('descending order', () => {
+		expect(
+			keepOnlyHigherRank([
+				{ name: 'Y', symbol: 'b', cmc_rank: 50 },
+				{ name: 'Z', symbol: 'U', cmc_rank: 5 },
+				{ name: 'B', symbol: 'u', cmc_rank: 4 },
+				{ name: 'b', symbol: 'B', cmc_rank: 3 },
+				{ name: 'a', symbol: 'A', cmc_rank: 2 },
+			])
+		).toEqual([
+			{ name: 'b', symbol: 'B', cmc_rank: 3 },
+			{
+				name: 'a',
+				symbol: 'A',
+				cmc_rank: 2,
+			},
+		])
+	})
 })
